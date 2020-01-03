e137dff0d5c21f6796b1c9fd4fd58b1346617efa
Meow <nyanhost@gmail.com>
2020-01-04 01:50:58 +0400

    Added get_url variable

--- imageChecker.template.conf.php
+++ imageChecker.template.conf.php
@@ -2,7 +2,7 @@
 // This file must be renamed to imageChecker.conf.php
 
 // Memcached server params
-$memcached = ['host' => '%HOST%', 'port' => 0, 'ttl' => 1200];
+$memcached = ['host' => '%HOST_OR_PATH_TO_MEMCACHED_SOCKET%', 'port' => 0, 'ttl' => 1200];
 
 // max. image size
 $max = [ 'size' => 1024*1024*5, 'width' => 8192, 'height' => 8192 ];
@@ -10,4 +10,5 @@ $max = [ 'size' => 1024*1024*5, 'width' => 8192, 'height' => 8192 ];
 // max. miniature size
 $miniature = [ 'maxwidth' => 165, 'maxheight' => 125 ];
 
+$get_url = "url";
 ?>
