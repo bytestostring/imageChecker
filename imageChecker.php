553dc92d0cc85c35686f96fa136df5a63d6506a1
Meow <nyanhost@gmail.com>
2020-01-04 02:14:20 +0400

    Update imageChecker.php

--- imageChecker.php
+++ imageChecker.php
@@ -2,6 +2,7 @@
 // Example of usage the script
 // https://yourhost.com/imageChecker?url={image_url} or
 // https://yourhost.com/imageChecker?mini=1&url={image_url} to get a miniature of image.
+
 	include('imageChecker.class.php');
 	if (!isset($_GET['url'])) {
 		http_response_code(403);
@@ -13,27 +14,32 @@
 		$ich->set_mini(true);
 	}
 
-	if (!isset($err)) {
-		if (!$ich->setLink($url)) {
-			$err = "An error to get the image";
+	$step = -1;
+	while (!isset($err) && $step < 3) {
+		$step++;
+		switch ($step) {
+			case 0:
+				if (!$ich->setLink($url)) {
+					$err = "An error to get the image";
+				}
+				break;
+			case 1:
+				if (!$ich->FastIdentifyImage()) {
+					$err = "An error to identify a type";
+				}
+				break;
+			case 2:
+				if (!$ich->getImage()) {
+					$err = "File size is too large";
+				}
+				break;
 		}
 	}
-	if (!isset($err)) {
-		if (!$ich->FastIdentifyImage()) {
-			$err = "An error to identify a type";
-		}
-	}
-	if (!isset($err)) {
-		if (!$ich->getImage()) {
-			$err = "File size is too large";
-		}
-	}
-
 	if (isset($err)) {
 		$ich->createError($err);
 		exit;
 	}
-		$ich->showImage();
-    
+	$ich->showImage();
+	
 ?>
 
